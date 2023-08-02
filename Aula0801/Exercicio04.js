bf0be537fc842c903ae135b231fe90fb9c10789b
Bernardo Mantoan Castellani <bernardo.castellani@alunos.bpkedu.com.br>
2023-08-01 22:16:19 -0300

    lista 2 0.1

---
+++ Aula0801/Exercicio04.js
@@ -0,0 +1,24 @@
+/*Crie um programa que gere uma senha aleatória com uma combinação de letras maiúsculas,
+minúsculas, números e caracteres especiais. Peça ao usuário para escolher o tamanho da
+senha.
+ */
+
+function geradorSenha(){
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-={}[]|:;"<>,.?/';
+    let senha = '';
+    
+
+    for (let i = 0; i < tamanhoSenha; i++) {
+        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+        senha += caracteres.charAt(indiceAleatorio);
+      }
+
+
+
+  return senha;
+}
+
+const tamanhoSenha = prompt('Digite o tamanho da senha desejada:');
+const senhaGerada = geradorSenha(tamanhoSenha);
+
+console.log('A senha gerada foi:', senhaGerada);
